d0510296a2d13d0d570c4ed9718c2902def20d26
AcevedoC17 <carlos.acevedo17@upr.edu>
2023-02-08 13:15:28 -0400

    Reworked ngrok boot logic

--- src/main.py
+++ src/main.py
@@ -604,18 +604,7 @@ class MyClient(commands.Bot):
               
 
 #----------------------------------------------------------------------------------------------------
-    def getNgrok(self):
-      '''subprocess.Popen(["ngrok","tcp", "22"],
-                        stdout=subprocess.PIPE)'''
-
-      time.sleep(3)  # to allow the ngrok to fetch the url from the server
-      localhost_url = "http://localhost:4040/api/tunnels"  # Url with tunnel details
-      tunnel_url = requests.get(localhost_url).text  # Get the tunnel information
-      j = json.loads(tunnel_url)
-
-      tunnel_url = j['tunnels'][0]['public_url']  # Do the parsing of the get
-      print(tunnel_url)
-      return tunnel_url.encode()
+    
 
     def query(self, payload):
         """
@@ -693,7 +682,7 @@ class MyClient(commands.Bot):
 
         self.admin = await client.fetch_user("129763563689476096")
         await self.admin.send("I'm trying to send you my latest IP")
-        await self.admin.send(self.getNgrok())
+        #await self.admin.send(self.getNgrok())
         
         
     
